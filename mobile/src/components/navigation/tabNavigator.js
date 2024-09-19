404060fe66660c6d379704a5c5e7fbcb35af3fd7
Hao <hao@gmail.com>
2024-09-19 20:54:45 +0700

    Update UI for  BottomTab Navigation Component

--- mobile/src/components/navigation/tabNavigator.js
+++ mobile/src/components/navigation/tabNavigator.js
@@ -1,11 +1,5 @@
 import { React, useState, useEffect } from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-} from "react-native";
+import { Dimensions, StyleSheet, Platform } from "react-native";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
@@ -19,97 +13,26 @@ import ProductHeader from "../header/productHeader";
 
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
-const screenWidth = Dimensions.get("screen").width;
+// const screenWidth = Dimensions.get("screen").width;
+
 const Tab = createBottomTabNavigator();
-
-const CustomTabBarIcon = ({ focused, routeName, size }) => {
-  // const [iconName, setIconName] = useState("");
-
-  // useEffect(() => {
-  //   if (routeName === "Trang chủ") {
-  //     setIconName(focused ? "home-outline" : "home-outline");
-  //   } else if (routeName === "Sản phẩm") {
-  //     setIconName(focused ? "add-circle" : "add-circle-outline");
-  //   } else if (routeName === "Đặt chỗ") {
-  //     setIconName(focused ? "appstore1" : "appstore-o");
-  //   } else if (routeName === "Hạng") {
-  //     setIconName(focused ? "appstore1" : "appstore-o");
-  //   } else if (routeName === "Khác") {
-  //     setIconName(focused ? "appstore1" : "appstore-o");
-  //   }
-  // }, [focused, routeName]);
-
-  const getIconComponent = () => {
-    if (routeName === "Trang chủ") {
-      return (
-        <MaterialCommunityIcons
-          name="home-outline"
-          color={focused ? "#93540A" : "#A8A8A8"}
-          size={size}
-        />
-      );
-    }
-    if (routeName === "Sản phẩm") {
-      return (
-        <MaterialCommunityIcons
-          name="coffee-outline"
-          color={focused ? "#93540A" : "#A8A8A8"}
-          size={size}
-        />
-      );
-    }
-    if (routeName === "Đặt chỗ") {
-      return (
-        <MaterialCommunityIcons
-          name="seat-outline"
-          color={focused ? "#93540A" : "#A8A8A8"}
-          size={size}
-        />
-      );
-    }
-    if (routeName === "Hạng") {
-      return (
-        <MaterialCommunityIcons
-          name="tag-heart-outline"
-          color={focused ? "#93540A" : "#A8A8A8"}
-          size={size}
-        />
-      );
-    }
-    if (routeName === "Khác") {
-      return (
-        <MaterialCommunityIcons
-          name="menu"
-          color={focused ? "#93540A" : "#A8A8A8"}
-          size={size}
-        />
-      );
-    }
-    return null;
-  };
-
-  return getIconComponent();
-};
 
 const BottomTabNavigator = () => {
   return (
     <Tab.Navigator
-      screenOptions={({ route }) => ({
-        // tabBarActiveTintColor: "blue",
-        // tabBarInactiveTintColor: "black",
+      screenOptions={() => ({
+        tabBarActiveTintColor: "#93540A",
+        tabBarInactiveTintColor: "#A8A8A8",
         tabBarStyle: styles.tabBar,
-        tabBarShowLabel: false,
-        tabBarIcon: ({ focused, color, size }) => (
-          <View style={styles.tabBarItem}>
-            <CustomTabBarIcon
-              routeName={route.name}
-              focused={focused}
-              color={color}
-              size={size}
-            />
-            <Text style={styles.tabBarLabel}>{route.name}</Text>
-          </View>
-        ),
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: "500",
+          ...Platform.select({
+            android: {
+              marginTop: -5,
+            },
+          }),
+        },
       })}
     >
       <Tab.Screen
@@ -117,6 +40,13 @@ const BottomTabNavigator = () => {
         component={home}
         options={{
           header: ({ navigation }) => <HomeHeader navigation={navigation} />,
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="home-outline"
+              color={focused ? "#93540A" : "#A8A8A8"}
+              size={24}
+            />
+          ),
         }}
       />
       <Tab.Screen
@@ -124,11 +54,54 @@ const BottomTabNavigator = () => {
         component={product}
         options={{
           header: ({ navigation }) => <ProductHeader navigation={navigation} />,
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="coffee-outline"
+              color={focused ? "#93540A" : "#A8A8A8"}
+              size={24}
+            />
+          ),
         }}
       />
-      <Tab.Screen name="Đặt chỗ" component={seatBooking} />
-      <Tab.Screen name="Hạng" component={rank} />
-      <Tab.Screen name="Khác" component={others} />
+      <Tab.Screen
+        name="Đặt chỗ"
+        component={seatBooking}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="seat-outline"
+              color={focused ? "#93540A" : "#A8A8A8"}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Hạng"
+        component={rank}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="tag-heart-outline"
+              color={focused ? "#93540A" : "#A8A8A8"}
+              size={24}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Khác"
+        component={others}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name="menu"
+              color={focused ? "#93540A" : "#A8A8A8"}
+              size={24}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
@@ -137,15 +110,13 @@ export default BottomTabNavigator;
 
 const styles = StyleSheet.create({
   tabBar: {
+    ...Platform.select({
+      android: {
+        height: 56,
+      },
+      ios: {},
+    }),
     backgroundColor: "#FFFFFF",
-    borderTopWidth: 0,
-  },
-  tabBarItem: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  tabBarLabel: {
-    color: "#A8A8A8",
-    fontSize: 10,
+    borderTopWidth: 0.5,
   },
 });
